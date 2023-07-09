90ba0671b3dd4ad5a0ecda9ddc8f94721b1fd422
Mariam <mariam.araqelyan2004@gmail.com>
2023-07-09 21:49:12 +0400

    basic calculator

---
+++ calculator.py
@@ -0,0 +1,37 @@
+while True:
+
+	a = (input("Enter the first number:"))
+	op = input("Choose the operation - (+, -, *, /):")
+	b = (input("Enter the second number:"))
+
+	if '.' in a:
+		a  = float(a)
+	else:
+		a = int(a)
+
+	if '.' in b:
+		b  = float(b)
+	else:
+		b = int(b)
+	
+	if op =='+':
+		print(f"{a} + {b} = {a + b}")
+	if op =='-':
+		print(f"{a} - {b} = {a - b}")
+	if op =='*':
+		print(f"{a} * {b} = {a * b}")
+	if op =='/':
+		if b == 0:
+			print("Can't divide on zero!")
+		else:
+			print(f"{a} / {b} = {a / b}")
+
+	answer = input("Do you want to exit, yes/no:")
+
+	if answer == "yes":
+		break
+ 
+	
+	
+
+
